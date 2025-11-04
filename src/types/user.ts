d030164b3f49743c92f3ba4e4096c9dbc006dce4
xamiir <cllaahicabdi04@gmail.com>
2025-11-04 18:57:13 +0300

    fix issue exist build

--- src/types/user.ts
+++ src/types/user.ts
@@ -10,11 +10,18 @@ export interface VerifyOTPPayload {
 
 export interface IUser {
   _id: string;
+  id: string;
   name: string;
   email: string;
   password: string;
   balance: number;
   apiKey: string;
+  phone?: string;
+  status?: string;
+  is_email_verified?: boolean;
+  is_2fa_enabled?: boolean;
+  created_at?: string;
+  updated_at?: string;
 }
 
 export type User = IUser;
@@ -25,4 +32,6 @@ export interface IUserDTO {
   password: string;
   balance?: number;
   apiKey: string;
+  phone?: string;
+  status?: string;
 }

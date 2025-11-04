f5b17f476fe9d5268561344f6f5149def8321b34
xamiir <cllaahicabdi04@gmail.com>
2025-11-04 18:55:40 +0300

    fix issue exist build

--- src/components/columns/category-columns.tsx
+++ src/components/columns/category-columns.tsx
@@ -4,7 +4,6 @@ import { Category } from "@/types";
 import { PATHS } from "@/routers/paths";
 import { EditIcon } from "lucide-react";
 import { EditButton } from "../authorized-buttons";
-import { Badge } from "../ui/badge";
 
 export const categoryColumns: ColumnDef<Category>[] = [
   {

7f0836387f1bdaf52fe2832fc8698fd5807bb268
xamiir <cllaahicabdi04@gmail.com>
2025-11-04 18:56:08 +0300

    fix issue exist build

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,8 +1,10 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+export type InputProps = React.ComponentProps<"input">;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -14,9 +16,9 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };

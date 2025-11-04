cfe21b6b6e3d18e34b0355436f58372aa4263adc
xamiir <cllaahicabdi04@gmail.com>
2025-11-04 18:55:58 +0300

    fix issue exist build

--- src/components/pages/entity-pie-chart.tsx
+++ src/components/pages/entity-pie-chart.tsx
@@ -63,7 +63,7 @@ export const EntityPieChart = observer(function EntityPieChart() {
               fill="#8884d8"
               dataKey="value"
             >
-              {data.map((entry, index) => (
+              {data.map((_, index) => (
                 <Cell
                   key={`cell-${index}`}
                   fill={COLORS[index % COLORS.length]}
